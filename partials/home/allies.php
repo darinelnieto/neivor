fb49b985c5d43fb14a6bdd7fa4e4b627dd4231fb
Darinel Nieto <darinelnietodiaz2021@gmail.com>
2025-03-04 18:47:33 -0500

    Aliados home

--- partials/home/allies.php
+++ partials/home/allies.php
@@ -13,12 +13,12 @@ if($allies):
 ?>
 <section class="allies-partial-c9ecc6">
     <div class="container">
-        <div class="row align-items-center">
+        <div class="row align-items-center justify-content-center">
             <div class="col-12">
                 <h2 class="title"><?= $allies['title']; ?></h2>
             </div>
             <?php if($allies['allies_list']): foreach($allies['allies_list'] as $item): ?>
-                <div class="col-6 col-md-3 col-lg-2 mb-4 mb-md-0 text-center">
+                <div class="col-6 col-md-3 mb-4 text-center">
                     <img src="<?= $item['logo']['url']; ?>" alt="<?= $item['logo']['title']; ?>">
                 </div>
             <?php endforeach; endif; ?>
