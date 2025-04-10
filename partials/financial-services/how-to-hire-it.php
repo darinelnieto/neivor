00fa3f3c6b3a729b68facf354529950608e035b2
Neivor <57572685+zyos-sas@users.noreply.github.com>
2025-04-10 12:39:21 -0500

    Update how-to-hire-it.php

--- partials/financial-services/how-to-hire-it.php
+++ partials/financial-services/how-to-hire-it.php
@@ -1,4 +1,3 @@
-   
 <?php
 /**
  * 
@@ -8,28 +7,32 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
-$manual = get_field('enable_manual');
-if($manual === true){
-    $cta = get_field('request_advice');
-    $title = get_field('how_to_hire_it_title');
-    $description = get_field('how_to_hire_it_description');
-}else{
-    $cta = get_field('request_advice', 'option');
-    $title = get_field('how_to_hire_it_title', 'option');
-    $description = get_field('how_to_hire_it_description', 'option');
+
+$cta = get_field('request_advice', 'option');
+
+// Obtener la URL actual
+$current_url = home_url( add_query_arg( [], $GLOBALS['wp']->request ) );
+
+// Definir redirección según URL actual
+$redirect_url = '#'; // Valor por defecto
+
+if ( strpos($current_url, 'agenda-demo-condominios') !== false ) {
+    $redirect_url = 'https://share.hsforms.com/1cl_nbLyjT-iy2QYDL6OASAcezkj';
+} elseif ( strpos($current_url, 'agenda-demo-rentas') !== false ) {
+    $redirect_url = 'https://share.hsforms.com/1h8uV6nSaQyqhw-WHmYOcxwcezkj';
 }
 ?>
+
 <section class="how-to-hire-it-partial-117e5e">
     <div class="container">
         <div class="row">
             <div class="col-12 card-purple">
-                <h2><?= $title; ?></h2>
-                <p><?= $description; ?></p>
-                <a href="<?= $cta['url']; ?>" target="<?= $cta['target']; ?>">
+                <h2><?= get_field('how_to_hire_it_title', 'option'); ?></h2>
+                <p><?= get_field('how_to_hire_it_description', 'option'); ?></p>
+                <a href="<?= $redirect_url; ?>" target="<?= $cta['target']; ?>">
                     <span><?= $cta['title']; ?></span>
                 </a>
             </div>
         </div>
     </div>
 </section>
-                    
