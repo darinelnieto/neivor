5c149d2f3aad8e8db420d865f6b40e0f99e61f14
Darinel Nieto <darinelnietodiaz2021@gmail.com>
2025-04-10 12:32:03 -0500

    Solucion baner how to hire it

--- partials/financial-services/how-to-hire-it.php
+++ partials/financial-services/how-to-hire-it.php
@@ -8,14 +8,23 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
-$cta = get_field('request_advice', 'option');
+$manual = get_field('enable_manual');
+if($manual === true){
+    $cta = get_field('request_advice');
+    $title = get_field('how_to_hire_it_title');
+    $description = get_field('how_to_hire_it_description');
+}else{
+    $cta = get_field('request_advice', 'option');
+    $title = get_field('how_to_hire_it_title', 'option');
+    $description = get_field('how_to_hire_it_description', 'option');
+}
 ?>
 <section class="how-to-hire-it-partial-117e5e">
     <div class="container">
         <div class="row">
             <div class="col-12 card-purple">
-                <h2><?= get_field('how_to_hire_it_title', 'option'); ?></h2>
-                <p><?= get_field('how_to_hire_it_description', 'option'); ?></p>
+                <h2><?= $title; ?></h2>
+                <p><?= $description; ?></p>
                 <a href="<?= $cta['url']; ?>" target="<?= $cta['target']; ?>">
                     <span><?= $cta['title']; ?></span>
                 </a>
