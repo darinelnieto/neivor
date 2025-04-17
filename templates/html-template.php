9d0b92cd4ca963d6bedbc88a10a6fb9a39725120
Neivor <57572685+zyos-sas@users.noreply.github.com>
2025-04-17 13:35:55 -0500

    Update html-template.php

--- templates/html-template.php
+++ templates/html-template.php
@@ -10,11 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 	<main id="html-template-6c9394">
-		<section class="htmlform-8190bc">
 		<?php get_template_part('partials/globals/html'); ?>
-		<?phpif($enable_form === true):?>
-			<?= $form; ?>
-		<?php endif; ?> 
-		</section>
 	</main>
 <?php get_footer(); ?> 
