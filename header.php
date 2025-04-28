ce85b53a74555a45d3ddf3a413525bca8590cda3
Neivor <57572685+zyos-sas@users.noreply.github.com>
2025-04-28 15:37:06 -0500

    Update header.php

--- header.php
+++ header.php
@@ -13,6 +13,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 <html class="no-js" <?php language_attributes(); ?>>
 
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTR4M74');</script>
+<!-- End Google Tag Manager -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTR4M74"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>	
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
@@ -22,14 +31,6 @@ if ( ! defined( 'ABSPATH' ) ) {
   <script>
     const _dittoURI_ = "<?= get_template_directory_uri() ?>",
           _dittoURL_ = "<?= get_site_url() ?>";
-  </script>
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-352383489"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'AW-352383489');
   </script>
   <!-- Meta Pixel Code -->
 <script>
