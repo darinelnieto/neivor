b75cd54818e7a5d8a0daac768e8e3760c3d5ffad
Neivor <57572685+zyos-sas@users.noreply.github.com>
2025-05-09 18:57:05 -0500

    Update html-template.php

--- templates/html-template.php
+++ templates/html-template.php
@@ -11,5 +11,8 @@ get_header();
 ?>
 	<main id="html-template-6c9394">
 		<?php get_template_part('partials/globals/html'); ?>
+
+
+		<?php get_template_part('partials/success-stories-single/related-post'); ?>
 	</main>
 <?php get_footer(); ?> 
